50afe19325bc92178cd30ef22932ad1d1880b5ff
sswen4sa <sswen4sa@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2010-12-31 06:07:21 +0000

    ClearTest is a unit testing framework leveraging F# quotations to make unit testing easier and more helpful in F# (the idea was inspired by the unit testing plugin we used in a grails project which used standard groovy asserts and pretty printed the value of step of an evaluation when a failure occurred... can't remember it's name). this frame work will support both interactive session and integrated visual studio runner support.

---
+++ ClearTest/trunk/ClearTest/Startup.fsx
@@ -0,0 +1,3 @@
+﻿#r @"C:\Program Files\FSharpPowerPack-2.0.0.0\bin\FSharp.PowerPack.Linq.dll"
+#load "TestOps.fs"
+open Swensen.ClearTest
---
+++ ClearTest/trunk/ClearTest/TestOps.fs
@@ -0,0 +1,47 @@
+﻿namespace Swensen.ClearTest
+
+[<AutoOpen>]
+module TestOps =
+    open Microsoft.FSharp.Quotations
+    open Microsoft.FSharp.Quotations.Patterns
+    open Microsoft.FSharp.Quotations.DerivedPatterns
+    open Microsoft.FSharp.Linq.QuotationEvaluation
+
+    //let OpExpr =
+    //    let eq = <@ (=) @>
+    //    let lt = <@ (<) @>
+    //    let gt = <@ (>) @>
+    //    let ltEq = <@ (<=) @>
+    //    let gtEq = <@ (>=) @>
+    //    let notEq = <@ (<>) @>
+
+    let rec sprintTestExpr expr =
+        match expr with
+        | Call (_, mi, lhs::rhs::[]) ->
+            let lhsValue, rhsValue = sprintTestExpr lhs, sprintTestExpr rhs
+            let opStr =
+                match mi.Name with
+                | "op_Equality" -> "="
+                | "op_GreaterThan" -> ">"
+                | "op_LessThan" -> "<"
+                | "op_GreaterThanOrEqual" -> ">="
+                | "op_LessThanOrEqual" -> "<="
+                | "op_Inequality" -> "<>"
+                | _ -> failwith "invalid test expression"
+
+            sprintf "%s %s %s" lhsValue opStr rhsValue
+
+        | _ -> sprintf "%A" (expr.EvalUntyped())
+
+    let test (expr:Expr<bool>) =
+        match expr.Eval() with
+        | false -> printfn "Test fails:\n\t%s" (sprintTestExpr expr)
+        | true -> ()
+
+    let inline (=?) x y = test <@ x = y @>
+    let inline (<?) x y = test <@ x < y @>
+    let inline (>?) x y = test <@ x > y @>
+    let inline (<=?) x y = test <@ x <= y @>
+    let inline (>=?) x y = test <@ x >= y @>
+    let inline (<>?) x y = test <@ x <> y @>
+
