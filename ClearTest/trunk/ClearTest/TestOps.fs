5dd22f6157391198c02ca5c7d8ea1eb9969c8625
sswen4sa <sswen4sa@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2011-01-03 03:10:11 +0000

    just more playing around

--- ClearTest/trunk/ClearTest/TestOps.fs
+++ ClearTest/trunk/ClearTest/TestOps.fs
@@ -9,32 +9,31 @@ module TestOps =
     open Microsoft.FSharp.Linq.QuotationEvaluation
 
 //    module OpExpr =
-//        let eq = <@ (=) @>
-//        let lt = <@ (<) @>
-//        let gt = <@ (>) @>
-//        let ltEq = <@ (<=) @>
-//        let gtEq = <@ (>=) @>
-//        let notEq = <@ (<>) @>
+//        let eq = <@@ (=) @@>
+//        let lt = <@@ (<) @@>
+//        let gt = <@@ (>) @@>
+//        let ltEq = <@@ (<=) @@>
+//        let gtEq = <@@ (>=) @@>
+//        let notEq = <@@ (<>) @@>
+
+    let binaryOps = [
+        "op_Equality", "="
+        "op_GreaterThan", ">"
+        "op_LessThan", "<"
+        "op_GreaterThanOrEqual", ">="
+        "op_LessThanOrEqual", "<="
+        "op_Inequality", "<>"
+        "op_PipeRight", "|>"
+        "op_PipeLeft", "<|"
+    ]
 
     //todo: expand to include +, -, *, etc.
     let (|BinaryInfixCall|_|) expr =
         match expr with
         | Call (_, mi, lhs::rhs::_) ->
-            let opStr =
-                match mi.Name with
-                | "op_Equality" -> "="
-                | "op_GreaterThan" -> ">"
-                | "op_LessThan" -> "<"
-                | "op_GreaterThanOrEqual" -> ">="
-                | "op_LessThanOrEqual" -> "<="
-                | "op_Inequality" -> "<>"
-                | "op_PipeRight" -> "|>"
-                | "op_PipeLeft" -> "<|"
-                | _ -> null
-
-            match opStr with
-            | null -> None
-            | opStr -> Some(opStr, lhs, rhs)
+            match binaryOps |> List.tryFind (fst>>((=) mi.Name)) with
+            | Some(_,opStr) -> Some(opStr,lhs,rhs)
+            | None -> None
         | _ -> None
 
     let rec sprintExpr expr =
@@ -54,18 +53,24 @@ module TestOps =
                     sprintf "%s.%s" pi.DeclaringType.Name pi.Name
         | Value(obj,_) ->
             sprintf "%A" obj
-        | _ -> sprintf "%A" (expr.EvalUntyped())
+        | NewUnionCase( _, _) ->
+            sprintf "%A" (expr.EvalUntyped())
+        | _ -> 
+            sprintf "%A" (expr.EvalUntyped())
 
     //this should return expr, with one one reduction applied
-    let rec reduce expr =
-        match expr with
-        | SpecificCall <@@ (=) @@> (_, _, lhs::rhs::_) ->
-            let lhsValue, rhsValue = reduce lhs, reduce rhs
-            sprintf "%s = %s" lhsValue rhsValue
-        | _ -> sprintf "%A" (expr.EvalUntyped()) 
-
+    let rec reduce (expr:Expr) = expr.EvalUntyped()
+        
     let reduceSteps (expr:Expr<bool>) =
-        seq {yield sprintExpr expr ; yield reduce expr}
+        let rec loop expr acc =
+            //let next = expr |> reduce |> sprintExpr
+            let next = expr |> sprintExpr
+            match next with
+            | "true" | "false" | _ when next = List.head acc -> acc
+            | _ -> loop expr (next::acc)
+
+        loop expr [expr |> sprintExpr] |> List.rev
+
     
     let fsiTestFailed (expr:Expr<bool>) =
         printfn "FALSE:" 
