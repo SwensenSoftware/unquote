660f73ec091fa6966d66197e0b0248e25d2f7bc4
sswen4sa <sswen4sa@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2011-01-09 18:21:34 +0000

    made progress on reduce, but need to clean things up / organize everything better.

--- ClearTest/trunk/ClearTest/TestOps.fs
+++ ClearTest/trunk/ClearTest/TestOps.fs
@@ -151,73 +151,77 @@ module TestOps =
             | None -> Some(mi,args)
         | _ -> None
 
+    let (|InstancePropertyGet|_|) expr =
+        match expr with
+        | PropertyGet(obj,mi,args) ->
+            match obj with
+            | Some(instance) -> Some(instance, mi, args)
+            | None -> None
+        | _ -> None
+
+    let (|StaticPropertyGet|_|) expr =
+        match expr with
+        | PropertyGet(obj,mi,args) ->
+            match obj with
+            | Some(_) -> None
+            | None -> Some(mi,args)
+        | _ -> None
+
     ///Construct a Value from an evaluated expression
     let evalValue (expr:Expr) = 
         let evaled = expr.EvalUntyped()
         Expr.Value(evaled, evaled.GetType())
 
-//    type reduceRes =
-//        | AlreadyValue
-//        | Evaled
-//        //| CouldNotEval
+    let isValue = function
+        | Value(_,_) -> true
+        | _ -> false
+
+    let allValues x = 
+        x |> List.filter isValue |> List.length = List.length x
 
     let rec reduce (expr:Expr) = 
-        match expr with
-        | Value(obj,objType) ->
-            Expr.Value(obj, objType)
-        | InstanceCall(calle,mi,args) ->
+        ///precondition: args is non-empty
+        let reduceFirstNonValueArg args =
             let argValues, argExprs = 
                 args 
                 |> List.mapi (fun i arg -> (i, arg))
                 |> List.partition (function |_,Value(_,_) -> true |_,_ -> false)
+            
+            let hd::tail = argExprs
+            let hdReduced = (fst hd, reduce (snd hd))
+            (argValues @ hdReduced::tail) |> List.sortBy fst |> List.map snd
 
-            match calle, argExprs with
-            | Value(_,_), [] -> //the calle and all args are Values
+        match expr with
+        | InstanceCall(calle,mi,args) ->
+            match isValue calle, allValues args with
+            | true, true -> //the calle and all args are Values
                 evalValue expr
-            | Value(_,_), hd::tail -> //the calle is a Value, but at least one arg is not, evaluate it
-                let hdValue = (fst hd, evalValue (snd hd))
-                Expr.Call(calle, mi, (argValues @ hdValue::tail) |> List.sortBy fst |> List.map snd)
-            | calle, _ -> //the calle is not a value
-                Expr.Call(evalValue calle, mi, args)
-//        | StaticCall(mi, args) ->
-//
-//            
+            | _, false -> //at least one arg is not a Value
+                Expr.Call(calle, mi, reduceFirstNonValueArg args )
+            | false, _ -> //the calle is not a value
+                Expr.Call(reduce calle, mi, args)
+        | StaticCall(mi,args) ->
+            match allValues args with
+            | true -> evalValue expr
+            | false -> Expr.Call(mi, reduceFirstNonValueArg args )
+        | InstancePropertyGet(calle,pi,args) ->
+            match isValue calle, allValues args with
+            | true, true -> //the calle and all args are Values
+                evalValue expr
+            | _, false -> //at least one arg is not a Value
+                Expr.PropertyGet(calle, pi, reduceFirstNonValueArg args )
+            | false, _ -> //the calle is not a value
+                Expr.PropertyGet(reduce calle, pi, args)
+        | StaticPropertyGet(pi,args) ->
+            match allValues args with
+            | true -> evalValue expr
+            | false -> Expr.PropertyGet(pi, reduceFirstNonValueArg args )
         | ShapeVar v -> 
             Expr.Var v
         | ShapeLambda (v,expr) -> 
             Expr.Lambda (v, reduce expr)
         | ShapeCombination (o, exprs) -> 
             RebuildShapeCombination (o, List.map reduce exprs)
-
-    //this should return expr, with one one reduction applied
-//    let reduce = 
-//        let mutable complete = false
-//        let rec reduce (expr:Expr) = 
-//            match expr with
-//            | Value(obj,objType) ->
-//                Expr.Value(obj, objType)
-//            | InstanceCall(calle,mi,args) ->
-//                if complete then Expr.Call(calle, mi, args)
-//                else
-//                    match calle, args |> List.filter (function |Value(_,_) -> false | _ -> true) with
-//                    | Value(_,_), [] -> 
-//                        compete <- true
-//                        evalValue expr
-//                    | Value(_,_), nonValueArgs ->
-//                        
-//
-//            | StaticCall(mi, args) ->
-//
-//            
-//            | ShapeVar v -> 
-//                Expr.Var v
-//            | ShapeLambda (v,expr) -> 
-//                Expr.Lambda (v, reduce expr)
-//            | ShapeCombination (o, exprs) -> 
-//                RebuildShapeCombination (o, List.map (reduce>>snd) exprs)
-//        
-//        fun expr -> reduce expr false
-
         
     let reduceSteps (expr:Expr<bool>) =
         let rec loop expr acc =
@@ -254,3 +258,15 @@ module TestOps =
     let inline (<=?) x y = test <@ x <= y @>
     let inline (>=?) x y = test <@ x >= y @>
     let inline (<>?) x y = test <@ x <> y @>
+
+    //for later
+    //test ops for combining expressions
+//    let inline (=?) x y = test <@ %x = %y @>
+//    let inline (<?) x y = test <@ %x < %y @>
+//    let inline (>?) x y = test <@ %x > %y @>
+//    let inline (<=?) x y = test <@ %x <= %y @>
+//    let inline (>=?) x y = test <@ %x >= %y @>
+//    let inline (<>?) x y = test <@ %x <> %y @>
+//
+//    let toListToExpr s =
+//        <@ List.ofSeq s @>
