f4119b87fcef1879d3a65a908bf0823f578ec8de
stephen.swensen@gmail.com <stephen.swensen@gmail.com@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2011-02-12 16:46:48 +0000

    first pass implimentation of raises<'a when 'a :> exn> (expr:Expr).  need to refactor testFailed so can reuse delegates for specialized message.

--- trunk/Unquote/Sprint.fs
+++ trunk/Unquote/Sprint.fs
@@ -497,3 +497,10 @@ let sprint expr =
 http://cs.hubfs.net/forums/thread/18361.aspx
 *)
 
+let simpleMatch =
+    <@
+        match true with
+        | true -> "true"
+        | _ -> "false"
+    @>
+
