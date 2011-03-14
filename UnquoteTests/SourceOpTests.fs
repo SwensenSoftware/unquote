b7ea3161a84081766a7096b370027113b7aacd53
stephen.swensen <stephen.swensen@gmail.com>
2011-03-14 11:14:56 +0000

    added test for tuples of length greater than 7

--- UnquoteTests/SourceOpTests.fs
+++ UnquoteTests/SourceOpTests.fs
@@ -48,6 +48,10 @@ let ``tuple of tuples (i.e. tuple containing sub-expressions)`` () =
     source <@ ((1,2,3), (2,3)) @> =? "((1, 2, 3), (2, 3))"
 
 [<Fact>]
+let ``tuple with length greater than 7``() =
+    source <@ (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18) @> =? "(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18)"
+
+[<Fact>]
 let ``literal list`` () =
     source <@ [1;2;3;] @> =? "[1; 2; 3]"
 
