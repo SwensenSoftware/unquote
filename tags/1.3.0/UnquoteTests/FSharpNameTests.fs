6f7b1d227ff4d86c2c269c0c3f9ea5c56e94cb20
stephen.swensen@gmail.com <stephen.swensen@gmail.com@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2011-04-03 18:27:44 +0000

    tagging for 1.3.0 release

---
+++ tags/1.3.0/UnquoteTests/FSharpNameTests.fs
@@ -0,0 +1,101 @@
+﻿(*
+Copyright 2011 Stephen Swensen
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*)
+
+[<AutoOpen>] //making auto open allows us not to have to fully qualify module properties
+module Test.Swensen.Unquote.FSharpNameTests
+open Xunit
+open Swensen.Unquote
+
+[<Fact>]
+let ``simple`` () =
+    typeof<float>.FSharpName =? "float";
+
+[<Fact>]
+let ``type with alias and generic args`` () =
+    typeof<list<float>>.FSharpName =? "list<float>";
+
+[<Fact>]
+let ``no alias with generic args`` () =
+    typeof<System.Collections.Generic.LinkedList<string>>.FSharpName =? "LinkedList<string>";
+
+[<Fact>]
+let ``no alias with no generic args`` () =
+    typeof<System.Collections.ArrayList>.FSharpName =? "ArrayList";
+
+[<Fact>]
+let ``tuple`` () =
+    typeof<int * int>.FSharpName =? "int * int";
+
+[<Fact>]
+let ``nested tuples`` () =
+    typeof<int * (float * (string * int))>.FSharpName =? "int * (float * (string * int))";
+
+[<Fact>]
+let ``generic tuple arg`` () =
+    typeof<list<int * int>>.FSharpName =? "list<int * int>";
+
+[<Fact>]
+let ``dynamic type test with complex type: nested and tuple precedence`` () =
+    typeof<int * list<float*(int * (string * float) * int)>>.FSharpName =? "int * list<float * (int * (string * float) * int)>";
+
+[<Fact>]
+let ``single dimimensional array of type alias`` () =
+    typeof<int[]>.FSharpName =? "int[]";        
+
+[<Fact>]
+let ``multi dimimensional array of type alias`` () =
+    typeof<int[,,]>.FSharpName =? "int[,,]";        
+
+[<Fact>]
+let ``jagged array of type alias`` () =
+    typeof<int[,,][][]>.FSharpName =? "int[,,][][]";
+
+[<Fact>]
+let ``array of no type alias`` () =
+    typeof<System.Text.RegularExpressions.Regex[]>.FSharpName =? "Regex[]";
+
+[<Fact>]
+let ``array of generic type`` () =
+    typeof<list<int>[]>.FSharpName =? "list<int>[]";
+
+[<Fact>]
+let ``tuple array is parenthisized`` () =
+    typeof<(int * int)[]>.FSharpName =? "(int * int)[]";
+
+[<Fact>]
+let ``fsharp funcs`` () =
+    typeof<int -> int * int -> float>.FSharpName =? "int -> int * int -> float";
+    typeof<int -> int * int -> float[]>.FSharpName =? "int -> int * int -> float[]";
+    typeof<list<int -> int * int -> float[]>>.FSharpName =? "list<int -> int * int -> float[]>";
+    typeof<Map<int, int -> int * int -> float[]>>.FSharpName =? "Map<int, int -> int * int -> float[]>";
+    typeof<(int -> int -> int)[][][]>.FSharpName =? "(int -> int -> int)[][][]";
+    typeof<(int -> int) -> int>.FSharpName =? "(int -> int) -> int";
+
+[<Fact>]
+let ``generic type definitions`` () =
+    typedefof<int -> (int * int)>.FSharpName =? "T -> TResult"
+    typedefof<int * int>.FSharpName =? "T1 * T2"
+    typedefof<list<_>>.FSharpName =? "list<T>"
+    typedefof<seq<list<_>>>.FSharpName =? "seq<T>"
+    typedefof<_[]>.FSharpName =? "obj[]"
+
+module Module = 
+    type Hello(x:int) = 
+        let x = x
+
+[<Fact>] //issue #10 
+let ``types in modules`` () =
+    typeof<Module.Hello>.FSharpName =? "Hello"
