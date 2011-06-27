1373a82f38551d409ce4b2ecb36aaf3747e4b1ea
stephen.swensen@gmail.com <stephen.swensen@gmail.com@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2011-06-27 23:03:28 +0000

    refactor for common naming convention

--- branches/v2-eval/Unquote.QuotationEvaluation/ExprExt.fs
+++ branches/v2-eval/Unquote.QuotationEvaluation/ExprExt.fs
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 *)
 [<AutoOpen>]
-module Swensen.Unquote.QuotationEvaluation.ExprExtensions
+module Swensen.Unquote.QuotationEvaluation.ExprExt
 
 open Microsoft.FSharp.Quotations
 
