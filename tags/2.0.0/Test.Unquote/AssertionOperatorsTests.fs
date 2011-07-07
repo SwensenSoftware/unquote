91687214e757859080f0bbae45143ef2dea7e7ab
stephen.swensen@gmail.com <stephen.swensen@gmail.com@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2011-07-07 03:24:25 +0000

    tagging 2.0.0 release

---
+++ tags/2.0.0/Test.Unquote/AssertionOperatorsTests.fs
@@ -0,0 +1,48 @@
+﻿(*
+Copyright 2011 Stephen Swensen
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*)
+
+[<AutoOpen>]
+module AssertionOperatorsTests
+open Xunit
+open Swensen.Unquote
+
+[<Fact>]
+let ``expect exception`` () =
+    raises<System.NullReferenceException> <@ (null:string).Length @>
+
+[<Fact>]
+let ``expect base exception`` () =
+    raises<exn> <@ (null:string).Length @>
+
+[<Fact>]
+let ``expect wrong exception`` () =
+    raises<exn> <@ raises<System.ArgumentException> <@ (null:string).Length @> @>
+
+[<Fact>]
+let ``raises no exception`` () =
+    raises<exn> <@ raises<exn> <@ 3 @> @>
+
+[<Fact>]
+let ``test passes`` () =
+    test <@ 4 = 4 @>
+
+[<Fact>]
+let ``test failes`` () =
+    raises<exn> <@ test <@ 4 = 5 @> @>
+
+
+
+
---
+++ tags/2.0.0/Utils/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Utils
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Swensen.Utils")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Swensen.Utils")>]
+[<assembly: AssemblyCopyright("Copyright © Stephen Swensen 2011")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly:System.Runtime.CompilerServices.InternalsVisibleTo("Unquote")>]
+[<assembly:System.Runtime.CompilerServices.InternalsVisibleTo("Test.Unquote")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("4d9645db-1c25-45ea-a6dd-5ad0d7768cac")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+()
