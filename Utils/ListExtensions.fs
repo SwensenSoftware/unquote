99dfce1762c7c4e43bb88c5476ba5475f915b34d
stephen.swensen <stephen.swensen@gmail.com>
2011-07-09 16:04:21 +0000

    more tuplelet refactoring

--- Utils/ListExtensions.fs
+++ Utils/ListExtensions.fs
@@ -16,6 +16,7 @@ limitations under the License.
 
 namespace Swensen.Utils
 module internal List =
+    ///Test whether the two lists are pairwise equal using the given boolean comparison function
     let rec equalsWith f xl yl =
         match xl, yl with
         | [], [] -> true
