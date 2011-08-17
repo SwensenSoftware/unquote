202c84d0c13c88cfa82154dc0826e8184e0a6c9b
stephen.swensen@gmail.com <stephen.swensen@gmail.com@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2011-08-17 02:06:34 +0000

    looking all good on the SL test front!

--- branches/silverlight/UnquoteTests.Silverlight/TestRunner.fs
+++ branches/silverlight/UnquoteTests.Silverlight/TestRunner.fs
@@ -3,25 +3,7 @@
 open Xunit
 open NUnit.Framework
 
-//does NUnit have a feature where I may treat one test as many?
-//the following would work but TestCaseSource not supported
-//[<TestFixture>]
-//type TestRunner() =
-//    let tests = [|
-//        let assm = System.Reflection.Assembly.GetExecutingAssembly()
-//        let tys = assm.GetTypes()
-//        for ty in tys do
-//            let methods = ty.GetMethods()
-//            for mi in methods do
-//                let attrs = mi.GetCustomAttributes(false)
-//                if attrs |> Array.exists (fun attr -> attr :? Xunit.FactAttribute) then 
-//                    yield [|mi|] |]
-//
-//    member this.Tests = tests 
-//
-//    [<Test; TestCaseSource("Tests")>]
-//    member this.Run(mi:System.Reflection.MethodInfo) =
-//        mi.Invoke(null,null) |> ignore
+//note: NUnit TestCaseSource not supported in Statlight so can't parameterize test
 
 [<TestFixture>]
 type TestRunner() =
