d558bb0b8071560af52723d811cb4c05d7ab63ca
stephen.swensen <stephen.swensen@gmail.com>
2011-09-24 21:46:30 +0000

    fixed issue with release mode silverlight tests not picking up Utils .dll in manifest: needed to add dummy reference to a Utils function since only other references were within quotations and so didn't get registered in test assembly.

--- run-silverlight-tests-debug.bat
+++ run-silverlight-tests-debug.bat
@@ -1,2 +1,2 @@
-C:\StatLight\statlight -d="C:\Users\Stephen\Documents\Visual Studio 2010\Projects\Unquote\code\UnquoteTests.Silverlight\bin\Debug\UnquoteTests.dll" -o=nunit
+C:\StatLight\statlight -d="C:\Users\Stephen\Documents\Visual Studio 2010\Projects\Unquote\code\UnquoteTests.Silverlight\bin\Debug\UnquoteTests.dll" -o=nunit --debug
 pause
