4fdf1d59dcaa05b745096b6073f3a8c236694f50
stephen.swensen@gmail.com <stephen.swensen@gmail.com@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2012-04-10 05:44:41 +0000

    use relative paths in silverlight test runners

--- trunk/run-silverlight-tests-debug.bat
+++ trunk/run-silverlight-tests-debug.bat
@@ -1,2 +1,2 @@
-packages\StatLight.1.4.4147\tools\statlight -d="C:\Users\Stephen\Documents\Visual Studio 2010\Projects\Unquote\code\UnquoteTests.Silverlight\bin\Debug\UnquoteTests.dll" -o=nunit --debug
+packages\StatLight.1.4.4147\tools\statlight -d="UnquoteTests.Silverlight\bin\Debug\UnquoteTests.dll" -o=nunit --debug
 pause
