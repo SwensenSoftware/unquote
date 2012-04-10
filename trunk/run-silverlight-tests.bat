b1d471fe4be2fd365aae2bef157be92a00ce1796
stephen.swensen@gmail.com <stephen.swensen@gmail.com@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2012-04-10 05:18:07 +0000

    update silverlight run scripts to use statlight nuget package

--- trunk/run-silverlight-tests.bat
+++ trunk/run-silverlight-tests.bat
@@ -1,2 +1,2 @@
-C:\StatLight\statlight -d="C:\Users\Stephen\Documents\Visual Studio 2010\Projects\Unquote\code\UnquoteTests.Silverlight\bin\Release\UnquoteTests.dll" -o=nunit --debug
+packages\StatLight.1.4.4147\tools\statlight -d="C:\Users\Stephen\Documents\Visual Studio 2010\Projects\Unquote\code\UnquoteTests.Silverlight\bin\Release\UnquoteTests.dll" -o=nunit --debug
 pause
