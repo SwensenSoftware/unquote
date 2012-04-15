e32820e02d01de7a688c76ea865800a6761379bd
stephen.swensen@gmail.com <stephen.swensen@gmail.com@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2012-04-15 21:10:21 +0000

    refactoring

--- branches/precompiled-regex/Unquote/RegexPatterns.fs
+++ branches/precompiled-regex/Unquote/RegexPatterns.fs
@@ -5,8 +5,9 @@ open Swensen.Unquote.PrecompiledRegexes
 
 module RegexPatterns =
     ///Match the numeric literal module name pattern and extract the suffix
-    let (|NumericLiteral|_|) input =
-        let regex = Swensen.Unquote.PrecompiledRegexes.NumericLiteralRegex()
-        match input with 
-        | Regex.RegexMatch regex {GroupValues=[suffix]} -> Some(suffix)
-        | _ -> None
+    let (|NumericLiteral|_|) =
+        fun input ->
+            let regex = Swensen.Unquote.PrecompiledRegexes.NumericLiteralRegex()
+            match input with 
+            | Regex.RegexMatch regex {GroupValues=[suffix]} -> Some(suffix)
+            | _ -> None
