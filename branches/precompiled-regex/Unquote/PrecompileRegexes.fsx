64587d5dcd4865d3e89b4918431e0586e966c744
stephen.swensen@gmail.com <stephen.swensen@gmail.com@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2012-04-15 16:38:16 +0000

    first steps on making precompiled regexes, but can't seem to set the CodeBase

---
+++ branches/precompiled-regex/Unquote/PrecompileRegexes.fsx
@@ -0,0 +1,15 @@
+﻿open System.Text.RegularExpressions
+
+let rcis = [|
+    new RegexCompilationInfo(
+        @"^NumericLiteral([QRZING])$",
+        RegexOptions.None,
+        "NumericLiteral",
+        "Swensen.Unquote.Regex",
+        true
+    );
+|]
+
+let an = new System.Reflection.AssemblyName("Unquote.Regex");
+an.CodeBase <- __SOURCE_DIRECTORY__  + "\\" + "Unquote.Regex.dll"
+Regex.CompileToAssembly(rcis, an)
