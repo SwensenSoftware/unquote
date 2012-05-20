7fb97489422ad6a51eefb6748c0ac8e066c8fff2
stephen.swensen@gmail.com <stephen.swensen@gmail.com@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2012-05-20 15:09:32 +0000

    * fix nuspec * update version numbers

--- trunk/Unquote.Silverlight/AssemblyInfo.fs
+++ trunk/Unquote.Silverlight/AssemblyInfo.fs
@@ -48,7 +48,7 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("2.1.1.*")>]
-[<assembly: AssemblyFileVersion("2.1.1.*")>]
+[<assembly: AssemblyVersion("2.2.0.*")>]
+[<assembly: AssemblyFileVersion("2.2.0.*")>]
 
 ()
