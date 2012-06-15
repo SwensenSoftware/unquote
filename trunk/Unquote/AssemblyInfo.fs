785066fa77b3bc45a08e371ac4be48bfbe9b02db
stephen.swensen@gmail.com <stephen.swensen@gmail.com@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2012-06-15 00:47:28 +0000

    update version numbers to 2.2.2 in prep for release. also updated copywrite and description info in assemblyinfo.cs files.

--- trunk/Unquote/AssemblyInfo.fs
+++ trunk/Unquote/AssemblyInfo.fs
@@ -23,11 +23,11 @@ open System.Runtime.InteropServices
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [<assembly: AssemblyTitle("Unquote")>]
-[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyDescription("Unquote for .NET 4.0")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("Unquote")>]
-[<assembly: AssemblyCopyright("Copyright © Stephen Swensen 2011")>]
+[<assembly: AssemblyCopyright("Copyright © Stephen Swensen 2011-2012")>]
 [<assembly: AssemblyTrademark("")>]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -48,7 +48,7 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("2.2.0.*")>]
-[<assembly: AssemblyFileVersion("2.2.0.*")>]
+[<assembly: AssemblyVersion("2.2.2.*")>]
+[<assembly: AssemblyFileVersion("2.2.2.*")>]
 
 ()
