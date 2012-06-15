79ac2e937e7f665ae8c4fb51272d460003dca366
stephen.swensen <stephen.swensen@gmail.com>
2012-06-15 00:47:28 +0000

    update version numbers to 2.2.2 in prep for release. also updated copywrite and description info in assemblyinfo.cs files.

--- Unquote.Silverlight/AssemblyInfo.fs
+++ Unquote.Silverlight/AssemblyInfo.fs
@@ -27,7 +27,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("Unquote")>]
-[<assembly: AssemblyCopyright("Copyright © Stephen Swensen 2011")>]
+[<assembly: AssemblyCopyright("Copyright © Stephen Swensen 2011-2012")>]
 [<assembly: AssemblyTrademark("")>]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -48,7 +48,7 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("2.2.0.*")>]
-[<assembly: AssemblyFileVersion("2.2.0.*")>]
+[<assembly: AssemblyVersion("2.2.2.*")>]
+[<assembly: AssemblyFileVersion("2.2.2.*")>]
 
 ()
