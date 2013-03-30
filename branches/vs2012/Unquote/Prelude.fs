b2540537bd7f0e3824019dac15db1b6f0647fb1e
stephen.swensen@gmail.com <stephen.swensen@gmail.com@fc8b7e07-5e15-1250-5e0c-899623c1da63>
2013-03-30 01:54:21 +0000

    branch trunk at rev 622 for vs2012 migration. initial migration, not yet compiling due to no silverlight 4 support. vs crashes soon after opening, don't know why.

---
+++ branches/vs2012/Unquote/Prelude.fs
@@ -0,0 +1,9 @@
+﻿[<AutoOpen>]
+[<System.ObsoleteAttribute>] //marking as obsolete is a workaround F# not honoring EditorBrowsable(EditorBrowsableState.Never) to hide intellisense discoverability, thanks to Tomas Petricek's answer on SO: http://stackoverflow.com/questions/6527141/is-it-possible-to-mark-a-module-function-as-hidden-from-intellisense-discovery/6527933#6527933
+module Swensen.Unquote.Prelude
+
+#nowarn "42" //for raises (inline IL)
+
+///raise is not inlined in Core.Operators, so shows up in stack traces.  We inline it here for clean stacktraces.
+let inline raise (e: System.Exception) = (# "throw" e : 'U #)
+
