c77e3b14770d31c76c37ef898f54f011d8d92318
stephen.swensen <stephen.swensen@gmail.com>
2015-04-12 17:50:19 +0000

    tests for unquotedexpression

--- UnquoteTests/UnquotedExpressionTests.fs
+++ UnquoteTests/UnquotedExpressionTests.fs
@@ -1,2 +1,30 @@
-﻿module UnquotedExpressionTests
+﻿[<AutoOpen>]
+module UnquotedExpressionTests
 
+open Xunit
+open Swensen.Unquote
+
+[<Fact>]
+let ``basic reduction`` () =
+    let x = <@ 2 + 2 @>
+    let ux = UnquotedExpression(x)
+    test <@ ux.ReductionException = None @>
+    test <@ ux.FinalReduction.Decompile() = "4" @>
+    test <@ ux.Reductions.Length = 2 @>
+    test <@ ux.DecompiledReductions = ["2 + 2"; "4"] @>
+
+[<Fact>]
+let ``framework reduction exception`` () =
+    let x = <@ 2 + 2; (null:string).ToString() @>
+    let ux = UnquotedExpression(x)
+    test <@ ux.ReductionException.Value.GetType() = typeof<System.NullReferenceException> @>
+    test <@ ux.FinalReduction.Type = typeof<ReductionException> @>
+    test <@ ux.Reductions.Length = 4 @>
+
+[<Fact>]
+let ``non-framework reduction exception`` () =
+    let x = <@ failwith "sorry" @>
+    let ux = UnquotedExpression(x)
+    test <@ ux.ReductionException.Value.GetType() = typeof<System.Exception> @>
+    test <@ ux.FinalReduction.Type = typeof<ReductionException> @>
+    test <@ ux.Reductions.Length = 2 @>
