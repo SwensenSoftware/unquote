e3af4601debc61ceadd4c0df54f2aced3f76ac27
stephen.swensen <stephen.swensen@gmail.com>
2015-04-20 03:18:22 +0000

    run-mono-tests.sh improved

--- run-mono-tests.sh
+++ run-mono-tests.sh
@@ -6,5 +6,6 @@ mono .nuget/nuget.exe restore Unquote.sln
 # mono packages/xunit.runner.console.2.0.0/tools/xunit.console.exe UnquoteTests/bin/Debug/UnquoteTests.dll
 
 # run .net 4.0 tests
-xbuild UnquoteTests.Net40/UnquoteTests.Net40.fsproj
-mono tools/xunit-1.9.2/xunit.console.clr4.exe "UnquoteTests.Net40/bin/Debug/UnquoteTests.Net40.dll"
+xbuild /p:Configuration=Release /t:Clean UnquoteTests.Net40/UnquoteTests.Net40.fsproj
+xbuild /p:Configuration=Release /p:DefineConstants="NET40" UnquoteTests.Net40/UnquoteTests.Net40.fsproj
+mono tools/xunit-1.9.2/xunit.console.clr4.exe "UnquoteTests.Net40/bin/Release/UnquoteTests.Net40.dll"
