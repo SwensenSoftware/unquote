59d13ef57ddf07a10a3d908545208c700b7e2bd0
Stephen Swensen <stephen.swensen@gmail.com>
2015-10-09 01:10:42 -0400

    get net45 tests running

--- run-mono-tests.sh
+++ run-mono-tests.sh
@@ -1,9 +1,15 @@
 # restore nuget packages
 mono .nuget/nuget.exe restore Unquote.sln
 
-# run .net 4.5 tests (current broken due to https://github.com/xunit/xunit/issues/158)
-# xbuild UnquoteTests/UnquoteTests.fsproj
-# mono packages/xunit.runner.console.2.0.0/tools/xunit.console.exe UnquoteTests/bin/Debug/UnquoteTests.dll
+# run .net 4.5 tests
+xbuild /p:Configuration=Release /t:Clean UnquoteTests/UnquoteTests.fsproj
+xbuild /p:Configuration=Release /p:DefineConstants="MONO" UnquoteTests/UnquoteTests.fsproj
+mono packages/xunit.runner.console.2.1.0/tools/xunit.console.exe UnquoteTests/bin/Release/UnquoteTests.dll -noappdomain
+
+# run .net portable profile tests (currently not working, need to install PCL reference assemblies or something)
+#xbuild /p:Configuration=Release /t:Clean UnquoteTests.Profile259/UnquoteTests.Profile259.fsproj
+#xbuild /p:Configuration=Release /p:DefineConstants="PORTABLE%3bMONO" UnquoteTests.Profile259/UnquoteTests.Profile259.fsproj
+#mono packages/xunit.runner.console.2.1.0/tools/xunit.console.exe UnquoteTests.Profile259/bin/Release/UnquoteTests.Profile259.dll -noappdomain
 
 # run .net 4.0 tests
 xbuild /p:Configuration=Release /t:Clean UnquoteTests.Net40/UnquoteTests.Net40.fsproj
