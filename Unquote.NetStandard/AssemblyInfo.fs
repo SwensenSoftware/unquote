e0fa67f331d7480a12952571bfb5869659a918f1
Stephen Swensen <stephen.swensen@relayfoods.com>
2017-11-09 16:00:37 -0500

    update version numbers in prep for release

--- Unquote.NetStandard/AssemblyInfo.fs
+++ Unquote.NetStandard/AssemblyInfo.fs
@@ -38,5 +38,5 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 
 //use odd number revision number (fourth position) to indicate pre-release
-[<assembly: AssemblyVersion("3.2.0.0")>]
+[<assembly: AssemblyVersion("4.0.0.0")>]
 ()
--- Unquote/AssemblyInfo.fs
+++ Unquote/AssemblyInfo.fs
@@ -30,7 +30,7 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("Unquote")>]
-[<assembly: AssemblyCopyright("Copyright © Stephen Swensen 2011-2016")>]
+[<assembly: AssemblyCopyright("Copyright © Stephen Swensen 2011-2017")>]
 [<assembly: AssemblyTrademark("")>]
 
 [<assembly:System.Runtime.CompilerServices.InternalsVisibleTo("UnquoteTests")>]
@@ -41,5 +41,5 @@ open System.Runtime.InteropServices
 [<assembly: ComVisible(false)>]
 
 //use odd number revision number (fourth position) to indicate pre-release
-[<assembly: AssemblyVersion("3.2.0.0")>]
+[<assembly: AssemblyVersion("4.0.0.0")>]
 ()
