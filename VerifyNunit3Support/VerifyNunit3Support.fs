4eac763113ae7746a61445c496ee3a48a18fcf3e
Stephen Swensen <stephen@imperfectproduce.com>
2019-06-24 00:19:12 -0400

    refactoring

--- VerifyNunit3Support/VerifyNunit3Support.fs
+++ VerifyNunit3Support/VerifyNunit3Support.fs
@@ -20,5 +20,5 @@ open NUnit
 open NUnit.Framework
 
 [<Test>]
-let ``test nunit support, this should fail with clean stack trace`` () =
+let ``test nunit support, this should fail with clean stack trace (but doesn't)`` () =
     test <@ 22 + 2 = 5 @>
