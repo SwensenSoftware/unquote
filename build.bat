caf3ded13ba41c1eb43710a74223ec0b9e99004f
Stephen Swensen <stephen@imperfectproduce.com>
2019-06-27 00:38:41 -0400

    progress revamping build script

--- build.bat
+++ build.bat
@@ -3,21 +3,14 @@ set versionNumber=%1
 del builds\Unquote-%versionNumber%.zip
 del builds\Unquote.%versionNumber%.nupkg
 
-REM preparing staging dirs
+REM preparing build dir
 
 mkdir builds
-mkdir staging
-mkdir staging\net45
-copy LICENSE staging
-copy NOTICE staging
 
 REM building for net45 ...
-"C:\Program Files (x86)\Microsoft SDKs\F#\4.0\Framework\v4.0\fsc.exe" -o:staging\net45\Unquote.dll --noframework --define:TRACE --doc:staging\net45\Unquote.xml --optimize+ -r:"C:\Program Files (x86)\Reference Assemblies\Microsoft\FSharp\.NETFramework\v4.0\4.3.1.0\FSharp.Core.dll" -r:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\mscorlib.dll" -r:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.Core.dll" -r:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.dll" -r:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5\System.Numerics.dll" --target:library --warn:3 --warnaserror:76 --vserrors --validate-type-providers --LCID:1033 --utf8output --fullpaths --flaterrors --subsystemversion:6.00 --highentropyva+ Unquote\Utils\Prelude.fs Unquote\Utils\Regex.fs Unquote\Utils\Printf.fs Unquote\Utils\List.fs Unquote\Utils\Type.fs Unquote\AssemblyInfo.fs Unquote\DynamicOperators.fs Unquote\EvaluationException.fs Unquote\Evaluation.fs Unquote\OperatorPrecedence.fs Unquote\ExtraReflection.fs Unquote\ExtraPatterns.fs Unquote\ReductionException.fs Unquote\Decompilation.fs Unquote\Reduction.fs Unquote\UnquotedExpression.fs Unquote\Extensions.fs Unquote\Operators.fs Unquote\AssertionFailedException.fs Unquote\Assertions.fs 
 
-REM zip builds ...
-cd staging
-..\tools\7z\7za.exe a -tzip ..\builds\Unquote-%versionNumber%.zip *
-cd ..
+".nuget\nuget.exe" install "Unquote\packages.config" -o packages
+msbuild ".\Unquote\Unquote.fsproj" /t:Clean,Build /p:Configuration=Release
 
 REM preparing nuget dirs
 
@@ -26,10 +19,10 @@ mkdir nuget\lib
 mkdir nuget\lib\net45
 copy Unquote.nuspec nuget
 
-REM copy staging builds to nuget package...
+REM copy builds to nuget dir...
 
-copy staging\net45\Unquote.dll nuget\lib\net45\Unquote.dll
-copy staging\net45\Unquote.xml nuget\lib\net45\Unquote.xml
+copy Unquote\bin\Release\Unquote.dll nuget\lib\net45\Unquote.dll
+copy Unquote\bin\Release\Unquote.xml nuget\lib\net45\Unquote.xml
 
 REM create nuget package...
 
@@ -57,5 +50,3 @@ if "%ERRORLEVEL%" == "0" (
 REM cleanup...
 
 rd /q /s nuget
-rd /q /s staging
-
